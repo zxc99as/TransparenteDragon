fe53b56ea9a75fac93db20ef7f1f4cf2c4069729
MIT-PC\zxc99as <zxc99as@naver.com>
2017-08-18 16:07:11 +0900

    이그노어 설정 중

---
+++ TransparenteDragon/Assets/Scripts/CameraSetting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetting : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject m_backgroud;
+
+	void Start()
+	{
+		Screen.SetResolution(Screen.width, Screen.height, true);
+		m_backgroud.transform.localScale = new Vector3(Screen.width, Screen.height, 1.0f);
+	}
+
+	void Update()
+	{
+
+	}
+}
