2128b0acc6740b43765fd8833ec8f4c19cc47076
MIT-PC\zxc99as <zxc99as@naver.com>
2017-08-21 17:48:29 +0900

    * 박스 콜라이더 제거, 좌표로 충돌 체크

--- TransparenteDragon/Assets/Scripts/UI/Button.cs
+++ TransparenteDragon/Assets/Scripts/UI/Button.cs
@@ -2,23 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Assets.Scripts.Utility;
 
 public class Button : MonoBehaviour
 {
 	[SerializeField]
 	protected Sprite[] m_images;
 
-	protected enum state : byte { IDLE = 0x00, CLICKED = 0x01, ON = 0x02, OFF = 0x03};
-	protected bool isMouseOver;
+	[SerializeField]
+	protected float m_width, m_height;
 
-	private void Start()
-	{
-		isMouseOver = false;
-	}
+	protected enum state : byte { IDLE = 0x00, CLICKED = 0x01, ON = 0x02, OFF = 0x03};
 
 	private void Update()
 	{
-		if (isMouseOver && Input.GetMouseButtonDown(0)) OnClick();
+		DebugUtil.DrawDebugBox(m_width, m_height, transform.position, Color.green);
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (ClickCheck())
+			{
+				OnClick();
+			}
+		}
 	}
 
 	protected void SetImage(state p_value)
@@ -27,13 +33,23 @@ public class Button : MonoBehaviour
 		gameObject.GetComponent<Image>().sprite = sprite;
 	}
 
+	protected bool ClickCheck()
+	{
+		Vector3 pos = transform.position;
+		Vector3 touchPos = Input.mousePosition;
+
+		touchPos = Camera.main.ScreenToWorldPoint(touchPos);
+
+		if (pos.x - m_width / 2 < touchPos.x && touchPos.x < pos.x + m_width / 2
+			&& pos.y - m_height / 2 < touchPos.y && touchPos.y < pos.y + m_height / 2)
+			return true;
+
+		else return false;
+	}
+
 	void OnClick()
 	{
 		Debug.Log(gameObject.name + " Clicked");
 		//SetImage(state.CLICKED);
 	}
-
-	protected void OnMouseOver() { isMouseOver = true; }
-	protected void OnMouseExit() { isMouseOver = false; }
-
 }
